91a60d1ab1e4d1fc9cf55dcdb89baac755d5687a
Thiago Souza <souzaalvesthiago99@gmail.com>
2023-01-11 12:22:24 -0300

    Criando sessão faq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
 
     for (let i = 0; i < buttons.length; i++) {
@@ -10,9 +11,21 @@ document.addEventListener('DOMContentLoaded', function(){
             tab.classList.add('shows__list--is-active');
             removeActiveButton();
             button.target.classList.add('shows__tabs__button--is-active');
+            questions.classList.add('faq__questions__item--is-open');
         })
     }
+
+    for (let i = 0; i< questions.length; i++) {
+        questions[i].addEventListener('click', openCloseAnswer);
+    }
 })
+
+function openCloseAnswer(elemento) {
+    const classe ='faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeActiveButton() {
     const buttons = document.querySelectorAll('[data-tab-button]');
