4523e70c7e104e4e08787a788af8218fac88fe4a
Thiago Souza <souzaalvesthiago99@gmail.com>
2023-01-12 17:55:15 -0300

    Criando header

--- src/main.js
+++ src/main.js
@@ -2,7 +2,21 @@ document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
     
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight;
 
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < heroHeight) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+
+    // Sessão atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button){
             const tabTarget = button.target.dataset.tabButton;
@@ -20,6 +34,17 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 })
 
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+// Sessão FAQ, accordion
 function openCloseAnswer(elemento) {
     const classe ='faq__questions__item--is-open';
     const elementoPai = elemento.target.parentNode;
